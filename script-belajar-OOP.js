aafb13403cc70679bf770181ca435c3d139f2ad9
irfannudin <irfannudin1998@gmail.com>
2025-04-13 21:18:08 +0900

    class public field

--- script-belajar-OOP.js
+++ script-belajar-OOP.js
@@ -167,19 +167,31 @@
 
 
 // (getter dan setter)
+// class Person {
+//     constructor(firstName, lastName){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+//     get fullName(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+//     set fullName(value){
+//         const result = value.split(" ");
+//         this.firstName = result[0];
+//         this.lastName = result[1];
+//     }
+// }
+// const eko = new Person("eko", "khanedy");
+// console.log(eko);
+// console.log(eko.fullName);
+
+
+
+
+
+// (PUBLIC CLASS FIELD)
 class Person {
-    constructor(firstName, lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    get fullName(){
-        return `${this.firstName} ${this.lastName}`;
-    }
-    set fullName(value){
-        const result = value.split(" ");
-        this.firstName = result[0];
-        this.lastName = result[1];
-    }
+    
 }
 const eko = new Person("eko", "khanedy");
 console.log(eko);
