0c2d02fdd0d21694e1fc498340690b7ece9380bc
irfannudin <irfannudin1998@gmail.com>
2025-04-24 20:04:14 +0900

    fungsi callback

--- script-belajar-asyncronus.js
+++ script-belajar-asyncronus.js
@@ -21,3 +21,22 @@
 
 // const meminta = mintaUang();
 // console.log(meminta);
+
+// fungsi call back = yaitu function yang dijadikan argument di function yang lain.(funsi dari callback functio adalah supaya function yang lambat tetap akan dieksekusi duluan sebelum function yang urutannya memang lebih terakhir.)
+// function haloKabar(callback){
+//      console.log('halo apa kabar!, salam kenal')
+//     callback();
+// };
+// function selamatJalan(){
+//     console.log('selamat jalan')
+// };
+// haloKabar(selamatJalan);
+// function penjumlahan(callback, a, b){
+//     let hasil = a+b;
+//     callback (hasil);
+// };
+// function consoleHasil(hasil){
+// console.log(hasil);
+// };
+
+// penjumlahan(consoleHasil,5,6);
