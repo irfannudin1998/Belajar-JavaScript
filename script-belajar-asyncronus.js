26b591b038324c7c0b052b7aa6c8c4b8449f2a19
irfannudin <irfannudin1998@gmail.com>
2025-04-24 13:08:22 +0900

    CALL BACK setInterval dan setTimeout

--- script-belajar-asyncronus.js
+++ script-belajar-asyncronus.js
@@ -1,1 +1,23 @@
-console.log('masuk')
+// (CALL BACK)
+// beberapa contoh async method
+// setTimeout(handler,time)
+// function menghadirkan(){
+//     console.log('saya hadir!!!')
+// };
+// function memanggil(){
+//     console.log("tolong hadirlah!!!")
+//     setTimeout(menghadirkan,5000)
+// };
+// console.log(memanggil());
+
+// setInterval(handler, time)
+// function uangBulanan(){
+//     console.log("ini uang untuk bulan depan ya!")
+// };
+// function mintaUang(){
+//     console.log('minta uang bulanannya donk pak');
+//     setInterval(uangBulanan,2000)
+// };
+
+// const meminta = mintaUang();
+// console.log(meminta);
