c0678ce971f7f2c4225576de30efda391cbd110d
irfannudin <irfannudin1998@gmail.com>
2025-04-26 15:21:24 +0900

    document DOM

--- belajar_DOM.js
+++ belajar_DOM.js
@@ -1,4 +1,15 @@
 // (pengertian DOM)
 // document object model = dom memungkinkan kita untuk mengubah struktur, isi, atau document tersebut melalui javascript
 
-// (tipe data)
+// document
+// console.log(document);
+
+// document property
+// console.log(document.body);
+// console.log(document.title);
+// console.log(document.images);
+// console.log(document.contentType);
+
+// document method
+const attackiing = document.getElementById('attack');
+console.log(attackiing);
