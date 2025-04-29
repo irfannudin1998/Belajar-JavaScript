c7dc84c6bbe9ee0e239b7f1415ca93be83b45444
irfannudin <irfannudin1998@gmail.com>
2025-04-29 11:28:17 +0900

    mendapatkan element berdasarkan id, class, tagname

--- belajar_DOM.js
+++ belajar_DOM.js
@@ -38,7 +38,22 @@
 // empat.textContent = "fourth"
 
 /* element property */
-const list = document.getElementById("list");
-console.log(list.tagName);
-console.log(list.id);
-console.log(list.className);
+// const list = document.getElementById("list");
+// console.log(list.tagName);
+// console.log(list.id);
+// console.log(list.className);
+
+/* cara memilih suatu element berdasarkan id, tag, dan class*/
+
+const tambahClass = document.getElementById("list");
+tambahClass.classList.add("kumpulanClass");
+const kumpulanClass1 = document.getElementsByClassName("kumpulanClass");
+const tambahClass2 = document.getElementById("satu");
+tambahClass2.style.backgroundColor = "tomato";
+document.getElementById("dua").classList.add("list2");
+const listKedua = document.getElementsByClassName("list2")[0];
+console.log(listKedua);
+listKedua.textContent = "ini dibuat dari class yang berasal dari js"
+const listKetiga = document.getElementsByTagName("li")[2];
+console.log(listKetiga);
+listKetiga.style.backgroundColor = "grey"
