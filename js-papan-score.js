cec43e17c8aee35adb05124a5638a380bb8fdc88
irfannudin <irfannudin1998@gmail.com>
2025-05-10 13:52:22 +0900

    membuat papan score

--- js-papan-score.js
+++ js-papan-score.js
@@ -1,1 +1,32 @@
-console.log("masuk")
+const player01 = document.querySelector("#SCORE-P1");
+const player02 = document.querySelector("#SCORE-P2");
+const maxPlayer = document.querySelector("#MAXSCORE");
+const reserButton = document.querySelector("#reset");
+
+let nilaiPlayer01 = 0;
+let nilaiPlayer02 = 0;
+let maxScore = 6;
+let gameOver = false;
+
+player01.addEventListener("click",()=>{
+    if(nilaiPlayer01>=maxScore||nilaiPlayer02>=maxScore){
+        if(Math.abs(nilaiPlayer01-nilaiPlayer02)>=2){
+            gameOver = true;
+        } else {
+        maxScore ++;
+        nilaiPlayer01 ++;    
+        }
+    }
+player01.textContent = nilaiPlayer01;
+})
+
+player02.addEventListener("click",()=>{
+     if(!gameOver){
+        nilaiPlayer02 ++;
+    }if(nilaiPlayer02===maxScore){
+        gameOver = true;
+    }
+player02.textContent = nilaiPlayer02;
+})
+
+
