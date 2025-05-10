8d14d915badac8bc347600384fd0f264213da530
irfannudin <irfannudin1998@gmail.com>
2025-05-10 14:20:06 +0900

    selesai membuat papan score

--- js-papan-score.js
+++ js-papan-score.js
@@ -8,6 +8,21 @@ let nilaiPlayer02 = 0;
 let maxScore = 6;
 let gameOver = false;
 
+function reset(){
+    gameOver = false;
+    nilaiPlayer01 = 0;
+    nilaiPlayer02 = 0;
+    player01.textContent = nilaiPlayer01;
+    player02.textContent = nilaiPlayer02;
+}
+
+maxPlayer.addEventListener("change", function(){
+    maxScore = parseInt(this.value);
+    reset()
+})
+
+reserButton.addEventListener("click",reset)
+
 player01.addEventListener("click",()=>{
       if(!gameOver){
         nilaiPlayer01 ++;
